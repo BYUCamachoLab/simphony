b63b10eef49e4b8b191b1c26caa487887e4aef9b
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-05 13:41:52 -0600

    Added new example to demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,10 +1,12 @@
 import simphony
 import matplotlib.pyplot as plt
 
-model = simphony.netlist.ObjectModelNetlist.load('output.json')
+model = simphony.netlist.ObjectModelNetlist.load('demo/output.json')
 sim = simphony.simulation.Simulation(model)
 
 x, y = sim.getMagnitudeByFrequencyTHz(2, 3)
 plt.plot(x, y)
 plt.show()
 
+multi = simphony.simulation.MultiInputSimulation(model)
+multi.multi_input_simulation(inputs=[2])
