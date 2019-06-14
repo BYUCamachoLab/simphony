98b3ee74969855ca5cef0d6f995413bd3fd2567b
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-14 11:57:46 -0600

    fix typo

--- simphony/elements/__init__.py
+++ simphony/elements/__init__.py
@@ -1,7 +1,7 @@
 __all__ = [
     'ebeam_bdc_te1550',
     'ebeam_dc_halfring_te1550',
-    'ebeam_gc_te1550'
+    'ebeam_gc_te1550',
     'ebeam_terminator_te1550',
     'ebeam_wg_integral_1550',
     'ebeam_y_1550',
