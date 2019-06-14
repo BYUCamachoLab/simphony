9edeed5cd16f17ecf87d2fb4a7ffc55a3927652f
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-14 09:55:40 -0600

    Added terminator component

---
+++ simphony/elements/ebeam_terminator_te1550/component.py
@@ -0,0 +1,40 @@
+from simphony import components
+
+# Create your component models here.
+
+class ebeam_terminator_te1550(components.BaseComponent):
+    """This class represents a terminator component in the netlist. All 
+    attributes can be initialized as keyword arguments in the __init__ 
+    function.
+
+    This class inherits from BaseComponent and inherits all of its data members.
+
+    Attributes
+    ----------
+    Inherited
+    """
+    def __init__(self, nets: list=[], lay_x: float=0, lay_y: float=0):
+        """Initializes a ebeam_terminator_te1550 dataclass, which inherits from 
+        BaseComponent.
+
+        Parameters
+        ----------
+        nets : list of ints
+            List of connected nets, ordered by port ordering.
+        lay_x : float
+            The x-position of the component in a layout.
+        lay_y : float
+            The y-position of the component in a layout.
+        """
+        super().__init__(nets=nets, lay_x=lay_x, lay_y=lay_y)
+
+    def get_s_parameters(self):
+        print(self.get_model())
+        # freq, sparams = self._model_ref.get_s_params(self.port_count)
+        # return simset.interpolate(freq, sparams)
+
+    class Metadata:
+        simulation_models = [
+            ('simphony.elements.ebeam_terminator_te1550.models', 'Terminator_Model'),
+        ]
+        ports = 1
