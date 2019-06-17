3028c11ac1208c4b311b8ced89a40f9d326fe602
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-17 08:00:58 -0600

    Rebuilt base components

--- simphony/core/base.py
+++ simphony/core/base.py
@@ -1,88 +1,157 @@
-"""
-components.py
-
-Author: Sequoia Ploeg
-
-Dependencies:
-- importlib
-    Dynamically imports the installed component models.
-
-This file dynamically loads all installed components within the models package.
-It also provides object models for netlist capabilities (all components 
-can be formatted as JSON).
-"""
-
 from importlib import import_module
+from typing import List, Dict
+import numpy
+from simphony.errors import DuplicateModelError
 
-class Component:
-    """This class represents an arbitrary type of component, but not an actual
-    instance of it.
+models = {}
+def clear_models():
+    """Clears all models loaded in program memory."""
+    models = {}
+    UniqueModelName._names = set()
 
-    Example: A ring resonator is a Component, but your layout may have
-    multiple ring resonators on it; those are represented by ComponentInstance.
+class UniqueModelName(object):
+    """Ensures that :obj:`ComponentModel <simphony.core.ComponentModel>` names 
+    (strings) are unique."""
+    _names = set()
 
-    This is the abstract base class for all components. All components
-    have a type and a list of simulation
-    models from which the component in question could retrieve s-parameters.
+    def __init__(self, name=None):
+        self.name = name
 
-    Attributes
-    ----------
-    component_type : str
-        The name of the component type. Component types are required to be
-        unique; two components with the same type are assumed to be identical
-        and have identical properties (width, height, etc.).
-    s_parameters : numpy.ndarray
-        The s-parameters of the component.
+    def __get__(self, obj, cls=None):
+        return self.name
 
-    Methods
-    -------
-    get_s_params(*args, **kwargs) : np.array, np.array
-        Abstract method that each class implements. Each classes passes in the
-        necessary parameters to its model and returns the frequency and 
-        s-parameter matrices.
+    def __set__(self, obj, value):
+        if value in self._names:
+            raise DuplicateModelError(value)
 
-    Notes
-    -----
-    get_s_params can be overridden. Simply write a function that returns or
-    calculates what you want, and then reassign the reference of the class
-    to your function.
+        self._add_name(value)
+        self.name = value
+
+    @classmethod
+    def _add_name(cls, name):
+        cls._names.add(name)
+
+class ComponentModel:
+    """The base class for all component models.
+    
+    This class represents the model for some arbitrary type of component, 
+    but not an actual instance of it. For example, a ring resonator is a 
+    ComponentModel, but your layout may have multiple ring resonators on it; 
+    those are represented by ComponentInstance.
+
+    Examples
+    --------
+    
+    Creation of a cachable component model:
+
+    >>> rr = simphony.core.ComponentModel("ring_resonator", s_params, cachable=True)
+
+    Creation of a non-cachable component model:
+
+    >>> wg = simphony.core.ComponentModel("waveguide", s_params, cachable=False)
+    >>> def new_s_parameters(length, width, thickness):
+    ...     # return some calculation based on parameters
+    >>> wg.get_s_parameters = new_s_parameters
     """
+    component_type = UniqueModelName()
 
-    def __init__(self, component_type, s_parameters, cachable=False):
-        """Initializes a Component dataclass.
+    def __init__(self, component_type, s_parameters=None, cachable=False):
+        """Initializes a ComponentModel dataclass.
 
+        A ComponentModel represents a type of component or device within a 
+        circuit. It is not an instance of that device, however; for example,
+        an electrical circuit can be constructed from resistors, transistors,
+        diodes, etc. But specific resistors, transistors, and diodes, and 
+        their locations or connections are specified as a ComponentInstance.
+
+        Parameters
+        ----------
+        component_type : str
+            A unique name specifying the type of this component.
+        s_parameters : numpy.array
+            A tuple, '(f,s)', where 'f' is the frequency array corresponding to
+            's', a matrix containing the s-parameters of the device.
+        cachable : bool
+            True if the s-parameters are static; false if they depend on other
+            variables.
+
+        Raises
+        ------
+        ValueError
+            If cachable=True and s_parameters are not specified.
+
+        See Also
+        --------
+        ComponentInstance : Component instances that reference ComponentModel.
         """
         self.component_type = component_type
-        self.s_parameters = s_parameters
+        if cachable:
+            if s_parameters is None:
+                raise ValueError("\'s_parameters\' cannot be None if cachable=True.")
+            self.s_parameters = s_parameters
         self.cachable = cachable
+        models[component_type] = self
 
-    def get_s_parameters(self, extras={}):
-        return self.s_parameters
+    def get_s_parameters(self, **kwargs) -> (numpy.array, numpy.array):
+        """Returns the s-parameters of the device.
+
+        By default, each ComponentModel takes s_parameters as a keyword 
+        argument upon instantiation. When this function is called, it simply 
+        returns the frequency and s-parameter matrices that were given. This
+        is only true if the model is 'cachable'. If not, then get_s_params can 
+        be easily overridden. Simply write a function that returns or 
+        calculates the s-parameters for the device (keyword arguments are 
+        allowed), and then reassign the `get_s_parameters` reference of the 
+        class post-instantiation.
+
+        Parameters
+        ----------
+        **kwargs
+            Derived models may require keyword arguments. See the documentation
+            for the models you are using.
+
+        Raises
+        ------
+        NotImplementedError
+            If the ComponentModel is not cachable.
+        """
+        if self.cachable:
+            return self.s_parameters
+        else:
+            raise NotImplementedError
 
     def __str__(self):
         return 'Object::' + str(self.__dict__)
 
     def __hash__(self):
-        return hash((self.component_type, self.cachable))
+        return hash((self.component_type, self.component_type))
 
     def __eq__(self, other):
+        # TODO: This method is really quite useless. If duplicate names
+        # can't be instantiated, we'll never be checking for equality.
+        """Checks two models for equality based on name and cachability."""
         if not isinstance(other, type(self)): return NotImplemented
-        return self.component_type == other.component_type
+        return self.component_type == other.component_type and \
+            self.cachable == other.cachable
+
+    def __copy__(self):
+        raise DuplicateModelError(self.component_type)
+
+    def __deepcopy__(self, memo):
+        raise DuplicateModelError(self.component_type)
 
 
 class ComponentInstance():
-    """
-    Attributes
-    ----------
-    nets : list of ints
-        A list of all connected nets (required to be integers) following the 
-        same order that the device's port numbering scheme follows.
-    lay_x : float
-        The x-position of the component in the overall layout.
-    lay_y : float
-        The y-position of the component in the overall layout.
+    """Create an instance in a circuit of an existing ComponentModel.
 
-    Other values can be passed in as "extras". For example, a waveguide needs
+    An ComponentInstance is a representation of an unique device within a
+    circuit or schematic. For example, while a resistor has a ComponentModel,
+    R1, R2, and R3 are instances of a resistor.
+
+    Notes
+    -----
+    Other values can be passed in as "extras". Extras are passed to 
+    "get_s_parameters" as keyword arguments. For example, a waveguide needs
     these values to properly calculate its s-parameters:
     length : float
         Total waveguide length.
@@ -93,10 +162,10 @@ class ComponentInstance():
     radius : float
         The bend radius of waveguide bends.
     points : list of tuples
-        A collection of all poitns which define the waveguides' path.
+        A collection of all points which define the waveguides' path.
     """
-    def __init__(self, component: Component=None, nets: list=[], lay_x: float=0, lay_y: float=0, extras: dict={}):
-        """Creates a physical instance of some BaseComponent.
+    def __init__(self, component: ComponentModel=None, nets: List=None, lay_x: float=0, lay_y: float=0, extras: Dict=None):
+        """Creates an instance of some ComponentModel.
 
         Parameters
         ----------
@@ -108,61 +177,11 @@ class ComponentInstance():
             The y-position of the component in the overall layout.
         """
         self.component = component
-        self.nets = nets
+        self.nets = nets if nets is not None else []
         self.lay_x = lay_x
         self.lay_y = lay_y
-        self.extras = extras
+        self.extras = extras if extras is not None else {}
 
     def get_s_parameters(self):
-        return self.component.get_s_parameters(self.extras)
-
-
-
-# from importlib import import_module
-# import pkgutil, inspect
-# import simphony.elements
-
-# def import_submodules(package, recursive=True):
-#     """ Import all submodules of a module, recursively, including subpackages.
-
-#     Courtesy of https://stackoverflow.com/a/25562415/11530613
-
-#     :param package: package (name or actual module)
-#     :type package: str | module
-#     :rtype: dict[str, types.ModuleType]
-#     """
-#     if isinstance(package, str):
-#         package = import_module(package)
-#     results = {}
-#     for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-#         full_name = package.__name__ + '.' + name
-#         results[full_name] = import_module(full_name)
-#         if recursive and is_pkg:
-#             results.update(import_submodules(full_name))
-#     return results
-
-# def load_elements():
-#     # Courtesy of https://stackoverflow.com/a/15833780/11530613
-#     mods = inspect.getmembers(simphony.elements, inspect.ismodule)
-#     for mod in mods:
-#         # print(mod)
-#         import_submodules(mod[1])
-
-# load_elements()
-
-# LOADED_COMPONENTS = {}
-# for c in BaseComponent.__subclasses__():
-#     LOADED_COMPONENTS[c.__name__] = c
-
-# def get_all_components():
-#     return BaseComponent.__subclasses__()
-
-# def get_all_models():
-#     return SimulationModel.__subclasses__()
-
-# def component_verifier(component):
-#     if len(component.Metadata.simulation_models) == 0:
-#         raise ImportError(type(component).__name__ + " has no simulation models defined.")
-
-# def create_component_by_name(component_name: str):
-#     return LOADED_COMPONENTS[component_name]()
+        """Get the s-parameters from the linked ComponentModel."""
+        return self.component.get_s_parameters(**self.extras)
