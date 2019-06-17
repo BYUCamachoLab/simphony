48e756a8bcf61f0330cb803a821e0749adf0be24
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-17 08:01:58 -0600

    Netlist, demo currently on hold

--- demo/demo.py
+++ demo/demo.py
@@ -1,12 +1,17 @@
+# import simphony
+# import matplotlib.pyplot as plt
+
+# model = simphony.netlist.ObjectModelNetlist.load('demo/output.json')
+# sim = simphony.simulation.Simulation(model)
+
+# x, y = sim.getMagnitudeByFrequencyTHz(2, 3)
+# plt.plot(x, y)
+# plt.show()
+
+# multi = simphony.simulation.MultiInputSimulation(model)
+# multi.multi_input_simulation(inputs=[2])
+
 import simphony
-import matplotlib.pyplot as plt
 
-model = simphony.netlist.ObjectModelNetlist.load('demo/output.json')
-sim = simphony.simulation.Simulation(model)
-
-x, y = sim.getMagnitudeByFrequencyTHz(2, 3)
-plt.plot(x, y)
-plt.show()
-
-multi = simphony.simulation.MultiInputSimulation(model)
-multi.multi_input_simulation(inputs=[2])
+ring_resonator = simphony.core.Component("ring_resonator_radius_5", [1500,1550,1600], [0,0,0], True)
+waveguide = simphony.core.Component("waveguide_length_10", [1500,1550,1600], [0,0,0], False)
--- simphony/core/netlist.py
+++ simphony/core/netlist.py
@@ -3,9 +3,6 @@
 # Dependencies:
 #     - numpy
 #         Required for cascading s-matrices together.
-#     - simphony.models
-#         The ObjectModelNetlist requires this module in order to build Component 
-#         models.
 #     - jsons
 #         Similar to GSON in Java, serializes and deserializes custom models.
 #         Required to convert the ObjectModelNetlist to a file that can be saved 
@@ -17,12 +14,15 @@
 # This file contains everything related to netlist generation and modeling.
 
 # from simphony.components import BaseComponent, create_component_by_name
-from simphony.core.base import Component, ComponentInstance
+
 import jsons
 import json
 import copy
 import numpy as np
-import simphony.core.connect as rf
+from typing import List
+
+from simphony.core.base import ComponentModel, ComponentInstance
+from simphony.core import connect as rf
 
 class Netlist:
     """Represents a netlist.
@@ -30,18 +30,16 @@ class Netlist:
     Has component_set, a set, and instance_list, a list.
     """
 
-    def __init__(self, component_set: set=None, instance_list: list=None, net_count: int=0):
+    def __init__(self, components: List[ComponentInstance]=None, net_count: int=0):
 
-        self.component_set = set() if component_set is None else component_set
-        self.instance_list = [] if instance_list is None else instance_list
+        self.components = [] if components is None else components
         self.net_count = net_count
 
-    def add_instance(self, component_instance):
-        self.component_set.add(component_instance.component)
-        self.instance_list.append(component_instance)
+    def add_component(self, component):
+        self.components.append(component)
 
     def get_external_components(self):
-        return [component for component in self.component_set if (any(int(x) < 0 for x in component.nets))]
+        return [component for component in self.components if (any(int(x) < 0 for x in component.nets))]
 
     def toJSON(self) -> str:
         return jsons.dump(self, verbose=True, strip_privates=True)
@@ -66,13 +64,195 @@ class Netlist:
             except:
                 raise RuntimeError("Netlist could not load successfully.")
 
+
+class CircuitConnector:
+    def __init__(self, netlist=None):
+        self.netlist = netlist
+
+    # def _match_ports(net_id: str, component_list: list) -> list:
+    #     """
+    #     Finds the components connected together by the specified net_id (string) in
+    #     a list of components provided by the caller (even if the component is 
+    #     connected to itself).
+
+    #     Parameters
+    #     ----------
+    #     net_id : str
+    #         The net id or name to which the components being searched for are 
+    #         connected.
+    #     component_list : list
+    #         The complete list of components to be searched.
+
+    #     Returns
+    #     -------
+    #     [comp1, netidx1, comp2, netidx2]
+    #         A list (length 4) of integers with the following meanings:
+    #         - comp1: Index of the first component in the list with a matching 
+    #             net id.
+    #         - netidx1: Index of the net in the ordered net list of 'comp1' 
+    #             (corresponds to its column or row in the s-parameter matrix).
+    #         - comp2: Index of the second component in the list with a matching 
+    #             net id.
+    #         - netidx1: Index of the net in the ordered net list of 'comp2' 
+    #             (corresponds to its column or row in the s-parameter matrix).
+    #     """
+    #     filtered_comps = [component for component in component_list if net_id in component.nets]
+    #     comp_idx = [component_list.index(component) for component in filtered_comps]
+    #     net_idx = []
+    #     for comp in filtered_comps:
+    #         net_idx += [i for i, x in enumerate(comp.nets) if x == net_id]
+    #     if len(comp_idx) == 1:
+    #         comp_idx += comp_idx
+        
+    #     return [comp_idx[0], net_idx[0], comp_idx[1], net_idx[1]]
+
+    # def connect_circuit(self) -> (ComponentSimulation, list):
+    #     """
+    #     Connects the s-matrices of a photonic circuit given its ObjectModelNetlist
+    #     and returns a single 'ComponentSimulation' object containing the frequency
+    #     array, the assembled s-matrix, and a list of the external nets (strings of
+    #     negative numbers).
+
+    #     Returns
+    #     -------
+    #     ComponentSimulation
+    #         After the circuit has been fully connected, the result is a single 
+    #         ComponentSimulation with fields f (frequency), s (s-matrix), and nets 
+    #         (external ports: negative numbers, as strings).
+    #     list
+    #         A list of ComponentModel objects that contain an external port.
+    #     """
+    #     if netlist.net_count == 0:
+    #         return
+
+    #     component_list = [ComponentSimulation(component) for component in netlist.component_list]
+    #     for n in range(0, netlist.net_count + 1):
+    #         ca, ia, cb, ib = _match_ports(str(n), component_list)
+
+    #         #if pin occurances are in the same Cell
+    #         if ca == cb:
+    #             component_list[ca].s = rf.innerconnect_s(component_list[ca].s, ia, ib)
+    #             del component_list[ca].nets[ia]
+    #             if ia < ib:
+    #                 del component_list[ca].nets[ib-1]
+    #             else:
+    #                 del component_list[ca].nets[ib]
+
+    #         #if pin occurances are in different Cells
+    #         else:
+    #             combination = ComponentSimulation()
+    #             combination.f = component_list[0].f
+    #             combination.s = rf.connect_s(component_list[ca].s, ia, component_list[cb].s, ib)
+    #             del component_list[ca].nets[ia]
+    #             del component_list[cb].nets[ib]
+    #             combination.nets = component_list[ca].nets + component_list[cb].nets
+    #             del component_list[ca]
+    #             if ca < cb:
+    #                 del component_list[cb-1]
+    #             else:
+    #                 del component_list[cb]
+    #             component_list.append(combination)
+
+    #     return component_list[0], netlist.get_external_components()
+
+
+    # def strToSci(number) -> float:
+    #     """
+    #     Converts string representations of numbers written with abbreviated 
+    #     prefixes into a float with the proper exponent (e.g. '3u' -> 3e-6).
+
+    #     Parameters
+    #     ----------
+    #     number : str
+    #         The number to be converted, represented as a string.
+        
+    #     Returns
+    #     -------
+    #     float
+    #         The string converted to a float.
+    #     """
+    #     ex = number[-1]
+    #     base = float(number[:-1])
+    #     if(ex == 'm'):
+    #         return base * 1e-3
+    #     elif(ex == 'u'):
+    #         return base * 1e-6
+    #     elif(ex == 'n'):
+    #         return base * 1e-9
+    #     else:
+    #         return float(number(base) + ex)
+
+    # def get_sparameters(netlist: ObjectModelNetlist):
+    #     """
+    #     Gets the s-parameters matrix from a passed in ObjectModelNetlist by 
+    #     connecting all components.
+
+    #     Parameters
+    #     ----------
+    #     netlist: ObjectModelNetlist
+    #         The netlist to be connected and have parameters extracted from.
+
+    #     Returns
+    #     -------
+    #     s, f, externals, edge_components: np.array, np.array, list(str)
+    #         A tuple in the following order: 
+    #         ([s-matrix], [frequency array], [external port list], [edge components])
+    #         - s-matrix: The s-parameter matrix of the combined component.
+    #         - frequency array: The corresponding frequency array, indexed the same
+    #             as the s-matrix.
+    #         - external port list: Strings of negative numbers representing the 
+    #             ports of the combined component. They are indexed in the same order
+    #             as the columns/rows of the s-matrix.
+    #         - edge components: list of ComponentModel objects, which are the external
+    #             components.
+    #     """
+    #     combined, edge_components = connect_circuit(netlist)
+    #     f = combined.f
+    #     s = combined.s
+    #     externals = combined.nets
+    #     return (s, f, externals, edge_components)
+
+    class ComponentSimulation:
+        """
+        This class is a simplified version of a ComponentModel in that it only contains
+        an ordered list of nets, the frequency array, and the s-parameter matrix. 
+        It can be initialized with or without a ComponentModel model, allowing its 
+        attributes to be set after object creation.
+
+        Attributes
+        ----------
+        nets : list(str)
+            An ordered list of the nets connected to the ComponentModel
+        f : np.array
+            A numpy array of the frequency values in its simulation.
+        s : np.array
+            A numpy array of the s-parameter matrix for the given frequency range.
+        """
+        nets: list
+        f: np.array
+        s: np.array
+
+        def __init__(self, component=None):
+            """
+            Instantiates an object from a ComponentModel if provided; empty, if not.
+
+            Parameters
+            ----------
+            component : ComponentModel, optional
+                A component to initialize the data members of the object.
+            """
+            if component:
+                self.nets = copy.deepcopy(component.nets)
+                self.f, self.s = component.get_s_parameters()
+
+
 def create_component_by_name(comp):
     return None
 
 class ObjectModelNetlist:
     """
     The Parser class reads a netlist generated by the SiEPIC toolbox and uses 
-    various classes which inherit from 'models.components.Component' to create 
+    various classes which inherit from 'models.components.ComponentModel' to create 
     an object based model of a photonic circuit. 
     
     Each derived class is connected to a component model in 'models' that 
@@ -83,7 +263,7 @@ class ObjectModelNetlist:
     Attributes
     ----------
     component_list : list
-        A list of objects derived from 'models.components.Component' 
+        A list of objects derived from 'models.components.ComponentModel' 
         representing the photonic circuit.
     net_count : int
         A counter keeping track of the total number of nets in the circuit 
@@ -148,7 +328,7 @@ class ObjectModelNetlist:
 
     def _parse_line(self, line_elements: list):
         """ Parses a line from the netlist, already split into individual 
-        elements, and converts it into a new Component object.
+        elements, and converts it into a new ComponentModel object.
 
         Reads the elements on a line of the netlist (already delimited before 
         passed to _parse_line) and creates the appropriate object. Appends the 
@@ -230,180 +410,3 @@ class ObjectModelNetlist:
             
         
 
-def _match_ports(net_id: str, component_list: list) -> list:
-    """
-    Finds the components connected together by the specified net_id (string) in
-    a list of components provided by the caller (even if the component is 
-    connected to itself).
-
-    Parameters
-    ----------
-    net_id : str
-        The net id or name to which the components being searched for are 
-        connected.
-    component_list : list
-        The complete list of components to be searched.
-
-    Returns
-    -------
-    [comp1, netidx1, comp2, netidx2]
-        A list (length 4) of integers with the following meanings:
-        - comp1: Index of the first component in the list with a matching 
-            net id.
-        - netidx1: Index of the net in the ordered net list of 'comp1' 
-            (corresponds to its column or row in the s-parameter matrix).
-        - comp2: Index of the second component in the list with a matching 
-            net id.
-        - netidx1: Index of the net in the ordered net list of 'comp2' 
-            (corresponds to its column or row in the s-parameter matrix).
-    """
-    filtered_comps = [component for component in component_list if net_id in component.nets]
-    comp_idx = [component_list.index(component) for component in filtered_comps]
-    net_idx = []
-    for comp in filtered_comps:
-        net_idx += [i for i, x in enumerate(comp.nets) if x == net_id]
-    if len(comp_idx) == 1:
-        comp_idx += comp_idx
-    
-    return [comp_idx[0], net_idx[0], comp_idx[1], net_idx[1]]
-
-
-class ComponentSimulation:
-    """
-    This class is a simplified version of a Component in that it only contains
-    an ordered list of nets, the frequency array, and the s-parameter matrix. 
-    It can be initialized with or without a Component model, allowing its 
-    attributes to be set after object creation.
-
-    Attributes
-    ----------
-    nets : list(str)
-        An ordered list of the nets connected to the Component
-    f : np.array
-        A numpy array of the frequency values in its simulation.
-    s : np.array
-        A numpy array of the s-parameter matrix for the given frequency range.
-    """
-    nets: list
-    f: np.array
-    s: np.array
-
-    def __init__(self, component=None):
-        """
-        Instantiates an object from a Component if provided; empty, if not.
-
-        Parameters
-        ----------
-        component : Component, optional
-            A component to initialize the data members of the object.
-        """
-        if component:
-            self.nets = copy.deepcopy(component.nets)
-            self.f, self.s = component.get_s_parameters()
-
-
-def connect_circuit(netlist: ObjectModelNetlist) -> (ComponentSimulation, list):
-    """
-    Connects the s-matrices of a photonic circuit given its ObjectModelNetlist
-    and returns a single 'ComponentSimulation' object containing the frequency
-    array, the assembled s-matrix, and a list of the external nets (strings of
-    negative numbers).
-
-    Returns
-    -------
-    ComponentSimulation
-        After the circuit has been fully connected, the result is a single 
-        ComponentSimulation with fields f (frequency), s (s-matrix), and nets 
-        (external ports: negative numbers, as strings).
-    list
-        A list of Component objects that contain an external port.
-    """
-    if netlist.net_count == 0:
-        return
-
-    component_list = [ComponentSimulation(component) for component in netlist.component_list]
-    for n in range(0, netlist.net_count + 1):
-        ca, ia, cb, ib = _match_ports(str(n), component_list)
-
-        #if pin occurances are in the same Cell
-        if ca == cb:
-            component_list[ca].s = rf.innerconnect_s(component_list[ca].s, ia, ib)
-            del component_list[ca].nets[ia]
-            if ia < ib:
-                del component_list[ca].nets[ib-1]
-            else:
-                del component_list[ca].nets[ib]
-
-        #if pin occurances are in different Cells
-        else:
-            combination = ComponentSimulation()
-            combination.f = component_list[0].f
-            combination.s = rf.connect_s(component_list[ca].s, ia, component_list[cb].s, ib)
-            del component_list[ca].nets[ia]
-            del component_list[cb].nets[ib]
-            combination.nets = component_list[ca].nets + component_list[cb].nets
-            del component_list[ca]
-            if ca < cb:
-                del component_list[cb-1]
-            else:
-                del component_list[cb]
-            component_list.append(combination)
-
-    return component_list[0], netlist.get_external_components()
-
-
-def strToSci(number) -> float:
-    """
-    Converts string representations of numbers written with abbreviated 
-    prefixes into a float with the proper exponent (e.g. '3u' -> 3e-6).
-
-    Parameters
-    ----------
-    number : str
-        The number to be converted, represented as a string.
-    
-    Returns
-    -------
-    float
-        The string converted to a float.
-    """
-    ex = number[-1]
-    base = float(number[:-1])
-    if(ex == 'm'):
-        return base * 1e-3
-    elif(ex == 'u'):
-        return base * 1e-6
-    elif(ex == 'n'):
-        return base * 1e-9
-    else:
-        return float(number(base) + ex)
-
-def get_sparameters(netlist: ObjectModelNetlist):
-    """
-    Gets the s-parameters matrix from a passed in ObjectModelNetlist by 
-    connecting all components.
-
-    Parameters
-    ----------
-    netlist: ObjectModelNetlist
-        The netlist to be connected and have parameters extracted from.
-
-    Returns
-    -------
-    s, f, externals, edge_components: np.array, np.array, list(str)
-        A tuple in the following order: 
-        ([s-matrix], [frequency array], [external port list], [edge components])
-        - s-matrix: The s-parameter matrix of the combined component.
-        - frequency array: The corresponding frequency array, indexed the same
-            as the s-matrix.
-        - external port list: Strings of negative numbers representing the 
-            ports of the combined component. They are indexed in the same order
-            as the columns/rows of the s-matrix.
-        - edge components: list of Component objects, which are the external
-            components.
-    """
-    combined, edge_components = connect_circuit(netlist)
-    f = combined.f
-    s = combined.s
-    externals = combined.nets
-    return (s, f, externals, edge_components)
