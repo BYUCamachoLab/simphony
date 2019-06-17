611db5e6e4d1d72a515f9f4b1e23014091d93881
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-17 08:00:43 -0600

    Started custom error package

---
+++ simphony/errors.py
@@ -0,0 +1,13 @@
+
+class DuplicateModelError(AttributeError):
+    def __init__(self, component_type):
+        """Error for a duplicate component type.
+
+        Parameters
+        ----------
+        component_type : UniqueModelName
+            The object containing the name of the model that is a duplicate
+        errors : *
+            Any errors corresponding to this error.
+        """
+        super().__init__("Component with name \'" + component_type + "\' already exists.")
