d5bec5b18c4dfaf6f182bdb3ec6f8cdccbd68c09
Sequoia Ploeg <sequoiap4@gmail.com>
2019-06-18 18:06:33 -0600

    Demo update

--- demo/demo.py
+++ demo/demo.py
@@ -11,7 +11,7 @@
 # multi = simphony.simulation.MultiInputSimulation(model)
 # multi.multi_input_simulation(inputs=[2])
 
-import simphony
+import simphony.core as core
 
-ring_resonator = simphony.core.Component("ring_resonator_radius_5", [1500,1550,1600], [0,0,0], True)
-waveguide = simphony.core.Component("waveguide_length_10", [1500,1550,1600], [0,0,0], False)
+ring_resonator = core.Component("ring_resonator_radius_5", [1500,1550,1600], [0,0,0], True)
+waveguide = core.Component("waveguide_length_10", [1500,1550,1600], [0,0,0], False)
