f887f5425ed8d336cbd3fb2b22bde49680d073b6
Sequoia Ploeg <sequoiap4@gmail.com>
2019-07-26 12:05:43 -0600

    Core monitors registered models

--- simphony/core/__init__.py
+++ simphony/core/__init__.py
@@ -2,3 +2,14 @@
 from .base import *
 from .netlist import *
 from . import connect
+from simphony.errors import DuplicateModelError
+
+models = {}
+
+def register_component_model(cls):
+    if cls.__name__ not in models.keys():
+        models[cls.__name__] = cls
+    else:
+        raise DuplicateModelError(cls.__name__)
+    return cls
+        
