5b0d0cbd6d4cd2ff4fe1db9b66e8e68aa5a39d23
Sequoia Ploeg <sequoiap4@gmail.com>
2019-09-25 14:45:05 -0600

    Completed connections

--- examples/GM/gm.py
+++ examples/GM/gm.py
@@ -4,14 +4,14 @@ import simphony.core as core
 from simphony.core import ComponentInstance as inst
 # import simphony.errors as errors
 import simphony.DeviceLibrary.ebeam as dev
-import simphony.DeviceLibrary.ann as lib
+import simphony.DeviceLibrary.sipann as lib
 import simphony.simulation as sim
 
 inputs = [inst(dev.ebeam_gc_te1550) for _ in range(4)]
 wg1 = [inst(dev.ebeam_wg_integral_1550, extras={'length':100e-6}) for _ in range(4)]
 dc1 = [inst(dev.ebeam_bdc_te1550) for _ in range(2)]
 wg_inner1 = [inst(dev.ebeam_wg_integral_1550, extras={'length':100e-6}) for _ in range(2)]
-# crossover = inst(lib.crossover)
+crossover = inst(lib.sipann_dc_crossover1550)
 wg_inner2 = [inst(dev.ebeam_wg_integral_1550, extras={'length':100e-6}) for _ in range(2)]
 wg_outer = [inst(dev.ebeam_wg_integral_1550, extras={'length':300e-6}) for _ in range(2)]
 dc2 = [inst(dev.ebeam_bdc_te1550) for _ in range(2)]
@@ -20,5 +20,32 @@ outputs = [inst(dev.ebeam_gc_te1550) for _ in range(4)]
 
 connections = []
 for i in range(4):
-    connections.append([inputs[i], 1, wg1[i], 0])
+    connections.append([inputs[i], 0, wg1[i], 0])
 
+connections.append([wg1[0], 1, dc1[0], 1])
+connections.append([wg1[1], 1, dc1[0], 0])
+connections.append([wg1[2], 1, dc1[1], 1])
+connections.append([wg1[3], 1, dc1[1], 0])
+
+connections.append([wg_outer[0], 0, dc1[0], 3])
+connections.append(wg_outer[1], 0, dc1[1], 2)
+connections.append([wg_inner1[0], 0, dc1[0], 2])
+connections.append([wg_inner1[0], 0, dc1[1], 3])
+
+connections.append([wg_inner1[0], 1, crossover, 1])
+connections.append([wg_inner1[1], 1, crossover, 0])
+connections.append([crossover, 3, wg_inner2[0], 0])
+connections.append([crossover, 2, wg_inner2[1], 0])
+
+connections.append([wg_outer[0], 1, dc2[0], 1])
+connections.append([wg_outer[1], 1, dc2[1], 0])
+connections.append([wg_inner2[0], 1, dc2[0], 0])
+connections.append([wg_inner2[1], 1, dc2[1], 1])
+
+connections.append([dc2[0], 3, wg3[0], 0])
+connections.append([dc2[0], 2, wg3[1], 0])
+connections.append([dc2[1], 3, wg3[2], 0])
+connections.append([dc2[1], 2, wg3[3], 0])
+
+for i in range(4):
+    connections.append([outputs[i], 0, wg3[i], 1])
