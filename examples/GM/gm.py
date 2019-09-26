eed723bf61df843c6749eec549c4ded7b9233937
Sequoia Ploeg <sequoiap4@gmail.com>
2019-09-26 14:05:16 -0600

    Added simulation visualizations

--- examples/GM/gm.py
+++ examples/GM/gm.py
@@ -28,7 +28,7 @@ connections.append([wg1[2], 1, dc1[1], 1])
 connections.append([wg1[3], 1, dc1[1], 0])
 
 connections.append([wg_outer[0], 0, dc1[0], 3])
-connections.append(wg_outer[1], 0, dc1[1], 2)
+connections.append([wg_outer[1], 0, dc1[1], 2])
 connections.append([wg_inner1[0], 0, dc1[0], 2])
 connections.append([wg_inner1[0], 0, dc1[1], 3])
 
@@ -49,3 +49,28 @@ connections.append([dc2[1], 2, wg3[3], 0])
 
 for i in range(4):
     connections.append([outputs[i], 0, wg3[i], 1])
+
+
+nl = core.Netlist()
+nl.load(connections, formatter='ll')
+simu = sim.Simulation(nl, num=1)
+
+
+freq = simu.freq_array
+zero2zero = np.log10(abs(simu.s_parameters()[:, 0, 0])**2)
+zero2one = np.log10(abs(simu.s_parameters()[:, 0, 1])**2)
+one2zero = abs(simu.s_parameters()[:, 1, 0])**2
+one2one = abs(simu.s_parameters()[:, 1, 1])**2
+
+import matplotlib.pyplot as plt
+plt.subplot(221)
+plt.plot(freq, zero2zero)
+plt.subplot(222)
+plt.plot(freq, zero2one)
+plt.subplot(223)
+plt.plot(freq, one2zero)
+plt.subplot(224)
+plt.plot(freq, one2one)
+plt.suptitle("MZI")
+plt.tight_layout()
+plt.show()
