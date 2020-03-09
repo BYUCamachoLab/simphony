75c589a59ba8ed08839a213d85a668f6d3995bf0
Sequoia Ploeg <sequoiap4@gmail.com>
2020-03-09 14:28:48 -0600

    New tests

--- tests/context.py
+++ tests/context.py
@@ -3,3 +3,9 @@
 # Copyright © Simphony Project Contributors
 # Licensed under the terms of the MIT License
 # (see simphony/__init__.py for details)
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import simphony
