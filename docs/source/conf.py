a015be0a99164aa94937340816a1e30166525046
Sequoia Ploeg <sequoiap4@gmail.com>
2020-04-10 16:09:30 -0600

    Removed some docs dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,9 +42,9 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.graphviz',
     'sphinx.ext.ifconfig',
-    'matplotlib.sphinxext.plot_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'IPython.sphinxext.ipython_directive',
+    # 'matplotlib.sphinxext.plot_directive',
+    # 'IPython.sphinxext.ipython_console_highlighting',
+    # 'IPython.sphinxext.ipython_directive',
     'sphinx.ext.imgmath',
     # 'sphinx.ext.todo', #
     # 'sphinx.ext.mathjax', #
