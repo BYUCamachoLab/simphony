9647e900e402b5619c82c9cdd4c3a44912a83dc9
Sequoia Ploeg <sequoiap4@gmail.com>
2020-05-19 12:28:35 -0600

    Remove gdsfactory which should be a separate library

--- simphony/library/gdsfactory/coupler_ring_siepic.py
+++
@@ -1,9 +0,0 @@
-import pp
-from simphony.library import siepic
-from simphony.library.gdsfactory import plot_sparameters
-
-if __name__ == "__main__":
-    c = siepic.ebeam_dc_halfring_straight(
-        gap=200e-9, radius=10e-6, width=500e-9, thickness=220e-9, couple_length=0.0
-    )
-    c = plot_sparameters(c)
