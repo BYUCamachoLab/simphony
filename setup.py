691ea658246c64d7af04b124e63e64ed591db40a
Sequoia Ploeg <36747422+sequoiap@users.noreply.github.com>
2020-08-06 12:11:13 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © Spyder Project Contributors
+# Copyright © Simphony Project Contributors
 # Licensed under the terms of the MIT License
-# (see spyder/__init__.py for details)
+# (see simphony/__init__.py for details)
 
 """Simphony Photonic Simulator.
 
