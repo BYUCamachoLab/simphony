aa459959bef2f8d5b7d4d614f72d7d9ab4e12d9a
Aust <austp17@gmail.com>
2021-05-25 15:38:19 -0600

    Update mzi.py example

--- examples/mzi.py
+++ examples/mzi.py
@@ -1,85 +1,64 @@
-#!/usr/bin/env python3
 # Copyright © Simphony Project Contributors
 # Licensed under the terms of the MIT License
 # (see simphony/__init__.py for details)
-#
-# File: mzi.py
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 from simphony.libraries import siepic
-from simphony.netlist import Subcircuit
-from simphony.simulation import SweepSimulation, MonteCarloSweepSimulation
+from simphony.simulators import MonteCarloSweepSimulator, SweepSimulator
 
-# Declare the models used in the circuit
-gc = siepic.ebeam_gc_te1550()
-y = siepic.ebeam_y_1550()
-wg150 = siepic.ebeam_wg_integral_1550(length=150e-6)
-wg50 = siepic.ebeam_wg_integral_1550(length=50e-6)
+# first we initialize all of the components in the MZI circuit
+gc_input = siepic.GratingCoupler()
+y_splitter = siepic.YBranch()
+wg_long = siepic.Waveguide(length=150e-6)
+wg_short = siepic.Waveguide(length=50e-6)
+y_recombiner = siepic.YBranch()
+gc_output = siepic.GratingCoupler()
 
-# Create the circuit, add all individual instances
-circuit = Subcircuit("MZI")
-e = circuit.add(
-    [
-        (gc, "input"),
-        (gc, "output"),
-        (y, "splitter"),
-        (y, "recombiner"),
-        (wg150, "wg_long"),
-        (wg50, "wg_short"),
-    ]
-)
+# next we connect the components to each other
+# you can connect pins directly:
+y_splitter["pin1"].connect(gc_input["pin1"])
 
-# You can set pin names individually:
-circuit.elements["input"].pins["n2"] = "input"
-circuit.elements["output"].pins["n2"] = "output"
+# or connect components with components:
+# (when using components to make connections, their first unconnected pin will
+# be used to make the connection.)
+y_splitter.connect(wg_long)
 
-# Or you can rename all the pins simultaneously:
-circuit.elements["splitter"].pins = ("in1", "out1", "out2")
-circuit.elements["recombiner"].pins = ("out1", "in2", "in1")
+# or any combination of the two:
+y_splitter["pin3"].connect(wg_short)
+# y_splitter.connect(wg_short["pin1"])
 
-# Circuits can be connected using the elements' string names:
-circuit.connect_many(
-    [
-        ("input", "n1", "splitter", "in1"),
-        ("splitter", "out1", "wg_long", "n1"),
-        ("splitter", "out2", "wg_short", "n1"),
-        ("recombiner", "in1", "wg_long", "n2"),
-        ("recombiner", "in2", "wg_short", "n2"),
-        ("output", "n1", "recombiner", "out1"),
-    ]
-)
+# when making multiple connections, it is often simpler to use `multiconnect`
+# multiconnect accepts components, pins, and None
+# if None is passed in, the corresponding pin is skipped
+y_recombiner.multiconnect(gc_output, wg_short, wg_long)
 
-# or by using the actual object reference.
-# circuit.connect(e[0], e[0].pin[0], e[2], e[2].pin[0])
+# instantiate the sweep simulator and connect it to our circuit
+# the first simulator pin connects to the input, the second to the output
+simulator = SweepSimulator(1500e-9, 1600e-9)
+simulator.multiconnect(gc_input, gc_output)
 
-# At this point, your circuit is defined. This file can serve as a description
-# for a subcircuit that is used in a larger circuit, and can simply be imported
-# using the Python import system (``from mzi import circuit``).
-
-# Run a simulation on the netlist.
-simulation = SweepSimulation(circuit, 1500e-9, 1600e-9)
-result = simulation.simulate()
-
-f, s = result.data("input", "output")
-plt.plot(f, s)
+# simulate and plot the results
+f, p = simulator.simulate()
+plt.plot(f, p)
 plt.title("MZI")
 plt.tight_layout()
 plt.show()
 
+# run a Monte Carlo simulation by disconnecting the previous simulator and
+# instantiating a MonteCarloSweepSimulator
+simulator.disconnect()
+simulator = MonteCarloSweepSimulator(1500e-9, 1600e-9)
+simulator.multiconnect(gc_input, gc_output)
 
-# We can run a monte carlo simulation on the circuit, too
-simulation = MonteCarloSweepSimulation(circuit, 1500e-9, 1600e-9)
-runs = 10
-result = simulation.simulate(runs=runs)
-for i in range(1, runs + 1):
-    f, s = result.data("input", "output", i)
-    plt.plot(f, s)
+# plot the values for 10 runs
+results = simulator.simulate(runs=10)
+for f, p in results:
+    plt.plot(f, p)
 
-# The data located at the 0 position is the ideal values.
-f, s = result.data("input", "output", 0)
-plt.plot(f, s, "k")
+# redraw the first results since they contain the ideal values
+f, p = results[0]
+plt.plot(f, p, "k")
 plt.title("MZI Monte Carlo")
 plt.tight_layout()
 plt.show()
