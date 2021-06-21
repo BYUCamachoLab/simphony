7e73891a270f2be6d600ed930497fc0f0edca729
Aust <austp17@gmail.com>
2021-06-21 11:45:04 -0600

    Remove custom lib example

--- examples/customlib.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Copyright © Simphony Project Contributors
-# Licensed under the terms of the MIT License
-# (see simphony/__init__.py for details)
-
-"""Coming Soon: A tutorial/best-practice-guide on creating your own model
-library."""
