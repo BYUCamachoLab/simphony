8d25e19861fe6c24089ca6250a5692e4cd657717
Water drinker <s39chand@edu.uwaterloo.ca>
2022-05-22 12:45:05 -0400

    Update test_layout_aware.py

--- simphony/tests/test_layout_aware.py
+++ simphony/tests/test_layout_aware.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import legend, plot, show
+from matplotlib.pyplot import legend, plot, show, xlabel, ylabel
 from numpy import log10, max
 import numpy as np
 import time
@@ -20,7 +20,7 @@ dc3 = siepic.DirectionalCoupler()
 dc4 = siepic.DirectionalCoupler()
 dc5 = siepic.DirectionalCoupler()
 
-components = (wg1, wg2, wg3, wg4, wg5, wg6, wg7, wg8, dc1, dc2, dc3, dc4, dc5)
+components = (dc1, wg1, wg2, dc2, wg3, wg4, dc3, wg5, wg6, dc4, wg7, wg8, dc5)
 
 dc1["pin3"].connect(wg1)
 dc1["pin4"].connect(wg2)
@@ -39,21 +39,20 @@ dc4["pin4"].connect(wg8)
 dc5["pin1"].connect(wg7)
 dc5["pin2"].connect(wg8)
 
-x = np.array([-2.14895e-04, -1.69565e-04, -1.69565e-04, -1.24235e-04, -7.89050e-05, -7.89050e-05, -3.35750e-05, 1.17550e-05, 1.17550e-05, 5.70850e-05, 1.02415e-04, 1.02415e-04, 1.47745e-04])
+x = np.array([-0.000214895, -0.000169565, -1.69565e-04, -1.24235e-04, -7.89050e-05, -7.89050e-05, -3.35750e-05, 1.17550e-05, 1.17550e-05, 5.70850e-05, 1.02415e-04, 1.02415e-04, 1.47745e-04])
 y = np.array([-4.574e-05, -3.4405e-05, -5.7009e-05, -4.574e-05, -3.4445e-05, -5.7009e-05, -4.574e-05, -7.647e-05, -4.0092e-05, -4.574e-05, -5.7009e-05, -2.1971e-05, -4.574e-05])
 
 simulator = LayoutAwareMonteCarloSweepSimulator()
-simulator.circuit = dc1.circuit
 simulator.multiconnect(dc1["pin1"], dc5["pin3"])
 
 start = time.time()
-results = simulator.simulate(x=x, y=y, sigmaw=5, sigmat=3, l=4.5e-3, runs=1000, dB=True)
+results = simulator.simulate(x=x, y=y, sigmaw=5, sigmat=2, l=4.5e-3, runs=10)
 end = time.time()
 print(f'{end-start}')
 g = []
 
 for wl, s in results:
-    g.append(10*s)
+    g.append(10*log10(s))
 
 for i, _g in enumerate(g):
     plot(wl, _g, label=f'{i}')
@@ -61,9 +60,37 @@ for i, _g in enumerate(g):
 legend()
 show()
 
-g = np.asarray(g)
-wl = np.asarray(wl)
-np.savetxt("Gain_1_simphony_test.txt", g)
-np.savetxt("wl.txt", wl)
+# g = np.asarray(g)
+# wl = np.asarray(wl)
+# np.savetxt("Gain_1_simphony_test2.txt", g)
+# np.savetxt("wl.txt", wl)
 
-print(max(g))
+# print(max(g))
+
+max = []
+# g = np.reshape(g, (2000,10))
+for i in range(len(g)):
+    max.append(np.max(g[i][:]))
+    print(np.max(g[i][:]))
+x = np.sort(max)
+
+y = 1. * np.arange(len(max)) / (len(max) - 1)
+
+plot(x, y, label="Simphony")
+
+data = np.loadtxt("C:\\Users\\12269\\Downloads\\Gain_1_VW.txt")
+data = data[4000:]
+data = np.reshape(data, (2000,1000))
+max = []
+
+for i in range(data.shape[1]):
+    max.append(np.max(data[:][i]))
+x = np.sort(max)
+
+y = 1. * np.arange(len(max)) / (len(max) - 1)
+
+plot(x, y, label="Lumerical")
+xlabel("max. Transmission (dB)")
+ylabel("Cumulative Probability")
+legend()
+show()
