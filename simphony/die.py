9bc37d4302fe44e82e918f4e5d2956c70c7feddd
SkandanC <s39chand@edu.uwaterloo.ca>
2022-07-10 04:19:28 -0400

    Component connections works!

--- simphony/die.py
+++ simphony/die.py
@@ -1,4 +1,5 @@
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Tuple
+import numpy as np
 from phidl.device_layout import Device, DeviceReference
 from phidl.geometry import grid
 import phidl.routing as pr
@@ -17,10 +18,12 @@ class Die(Device):
     def __init__(self, name:str = "", *args, **kwargs):
 
         self.device: Device = Device(name=name)
+        self.device_grid: Device = Device(name=f'{name}_grid')
         self.device_list: List[Device] = []
         self.device_refs: List[DeviceReference] = []
         self.connect_refs: List[DeviceReference] = []
         self.grid: List[Device] = []
+        self.spacing: Tuple = ()
         super().__init__(name=name, *args, **kwargs)
 
     def add_components(self, components: List["Model"]):
@@ -43,18 +46,13 @@ class Die(Device):
                 #     if device.name is component1.name or pin_._connection._component.name:
                 #         self.device_list.remove(device)
         elif not isinstance(pin1._connection._component, (siepic.Waveguide, Subcircuit, Simulation, Laser, Detector)):
-            # ref = self.device_refs[self.device_list.index(pin1._component.device)].connect(self.device_refs[self.device_list.index(pin1._component.device)].parent.ports[pin1.name], self.device_refs[self.device_list.index(pin2._component.device)].parent.ports[pin2.name])
-            # self.references[self.device_list.index(pin1._component.device)].connect(self.device_list[self.device_list.index(pin1._component.device)].ports[pin1.name], self.device_list[self.device_list.index(pin2._component.device)].ports[pin2.name])
-            self.device.references[self.device_list.index(pin1._component.device)].connect(self.device_list[self.device_list.index(pin1._component.device)].ports[pin1.name], self.device_list[self.device_list.index(pin2._component.device)].ports[pin2.name])
-            # self.device_refs[self.device_list.index(pin1._component.device)] = ref
-            # self.device_refs.pop(self.device_list.index(pin1._component.device))
-            # ref.parent.name = f'{component1}.{pin1.name}_{component2}.{pin2.name}'
-            # self.add_ref(ref.parent)
-            # if grid is not []:
-            #     self.grid.append(ref.parent)
-            # for device in self.device_list:
-            #     if device.name is component1.name or component2.name:
-            #         self.device_list.remove(device)
+            if self.device_grid_refs[self.device_list.index(component2.device)].parent.ports[pin2.name].orientation == 0 or 180:
+                overlap = np.linalg.norm(self.device_grid_refs[self.device_list.index(component2.device)].parent.ports[pin2.name].midpoint + self.device_grid_refs[self.device_list.index(component1.device)].parent.ports[pin1.name].midpoint) + self.spacing[0]
+                print(f'overlap={overlap}')
+            elif self.device_grid_refs[self.device_list.index(component2.device)].parent.ports[pin2.name].orientation == 90 or 270:
+                overlap = np.linalg.norm(self.device_grid_refs[self.device_list.index(component2.device)].parent.ports[pin2.name].midpoint + self.device_grid_refs[self.device_list.index(component1.device)].parent.ports[pin1.name].midpoint) + self.spacing[1]
+                print(f'overlap={overlap}')
+            self.device_grid_refs[self.device_list.index(component1.device)].connect(self.device_grid_refs[self.device_list.index(component1.device)].parent.ports[pin1.name], self.device_grid_refs[self.device_list.index(component2.device)].parent.ports[pin2.name], overlap=overlap)
 
     def distribute_devices(self, elements='all', direction="x", shape=None, spacing=100, separation=True):
         if direction == 'x':
@@ -64,7 +62,9 @@ class Die(Device):
             self.distribute(elements=elements, direction='y', spacing=spacing, separation=separation)
             self.grid = []
         elif direction == 'grid':
-            self.device = grid(device_list=self.device_list, spacing=(5, 10), separation=separation, shape=shape)
+            self.spacing = spacing
+            self.device_grid = grid(device_list=self.device_list, spacing=self.spacing, separation=separation, shape=shape)
+            self.device_grid_refs: DeviceReference = self.device_grid.references
 
     def write_gds(self, filename, unit=0.000001, precision=1e-9, auto_rename=True, max_cellname_length=28, cellname="toplevel"):
         return super().write_gds(filename, unit, precision, auto_rename, max_cellname_length, cellname)
@@ -73,4 +73,4 @@ class Die(Device):
         return super().write_svg(outfile, scaling, style, fontstyle, background, pad, precision)
 
     def visualize(self):
-        quickplot(self.device)
+        quickplot(self.device_grid_refs)
