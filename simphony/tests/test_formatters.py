f657a97874260e3da37ec998a4ea2d877f4bc2f6
SkandanC <s39chand@edu.uwaterloo.ca>
2022-08-25 08:42:19 -0400

    Fix test

--- simphony/tests/test_formatters.py
+++ simphony/tests/test_formatters.py
@@ -146,5 +146,11 @@ class TestCircuitSiEPICFormatter:
         )
 
         mzi42 = Circuit.from_file(spi, formatter=CircuitSiEPICFormatter())
-        print(mzi4.s_parameters(freqs) - mzi42.s_parameters(freqs))
-        assert np.allclose(mzi4.s_parameters(freqs), mzi42.s_parameters(freqs))
+        
+        _, p1 = mzi4._get_components()[-1].simulate()
+
+        mzi42._get_components()[-1].freqs = freqs
+
+        _, p2 = mzi42._get_components()[-1].simulate()
+
+        assert np.allclose(p1, p2)
