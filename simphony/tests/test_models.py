ade8a01fefa97541893225f93f4c58f3b7878487
SkandanC <s39chand@edu.uwaterloo.ca>
2022-10-26 20:39:50 -0400

    Revert "Merge branch 'add-3.11-to-workflows' into pr/66"
    This reverts commit 35739af27977062bf1ca2f1224f54f99b40fa752, reversing
    changes made to e07d61a2fa8dc1e08973c9db2ebe8fb227afd3ef.

--- simphony/tests/test_models.py
+++ simphony/tests/test_models.py
@@ -128,5 +128,4 @@ class TestModelComponent:
     def testcomponent(self, y1):
         y2 = siepic.YBranch(name="y2")
 
-        if hasattr(y1, "component"):
-            assert y1.component != y2.component
+        assert y1.component != y2.component
