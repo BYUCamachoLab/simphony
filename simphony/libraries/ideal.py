ee1f7d052951d14c6acf30086b812c4c31f74368
Christian Carver <christianjcarv@gmail.com>
2023-03-06 14:11:39 -0700

    Add docstring to coupler

--- simphony/libraries/ideal.py
+++ simphony/libraries/ideal.py
@@ -16,6 +16,34 @@ from simphony.context import CTX
 
 
 class Coupler(Model):
+    r"""
+    2x2 photonic coupler model.
+
+    The coupler has 2 inputs ('o1', 'o2') and 2 outputs ('o3', 'o4').
+    The coupler has the following s-parameter matrix:
+
+    .. math::
+        M = \begin{bmatrix}
+                0 & 0 & t \sqrt{T_0 T_2} & r^* \sqrt{T_0 T_3} \\
+                0 & 0 & -r \sqrt{T_1 T_2} & t \sqrt{T_1 T_3} \\
+                t \sqrt{T_0 T_2} & -r^* \sqrt{T_1 T_2} & 0 & 0 \\
+                r \sqrt{T_0 T_3} & t \sqrt{T_1 T_3} & 0 & 0
+            \end{bmatrix}
+
+    where :math:`t = \sqrt{1 - \text{coupling}}` and :math:`r = \sqrt{\text{coupling}}`.
+
+
+    Parameters
+    ----------
+    coupling : float
+        Coupling coefficient (0 <= coupling <= 1)
+    phi : float
+        Phase shift between the two output ports (in radians)
+    loss : float or list of floats
+        Loss of the component in dB (0 <= loss). If a list of 4 floats is given,
+        the loss associated with each port is set individually.
+    """
+    jit = False
     onames = ["o1", "o2", "o3", "o4"]
 
     def __init__(
@@ -27,9 +55,9 @@ class Coupler(Model):
         self.coupling = coupling
         self.phi = phi
         if hasattr(loss, "__iter__"):
-            self.T0, self.T1, self.T2, self.T3 = loss
+            self.T0, self.T1, self.T2, self.T3 = 10**(loss/10)
         else:
-            self.T0 = self.T1 = self.T2 = self.T3 = loss ** (1 / 4)
+            self.T0 = self.T1 = self.T2 = self.T3 = 10**(loss/10) ** (1 / 4)
         self.t = jnp.sqrt(1 - self.coupling)
         self.r = jnp.sqrt(self.coupling)
         self.rp = jnp.conj(self.r)
@@ -44,12 +72,12 @@ class Coupler(Model):
                 [0, 0, -r * jnp.sqrt(T1*T2),  t * jnp.sqrt(T1*T3)],
                 [t * jnp.sqrt(T0*T2), -rp * jnp.sqrt(T1*T2), 0, 0],
                 [r * jnp.sqrt(T0*T3), t * jnp.sqrt(T1*T3), 0, 0]
-            ]
+            ], dtype=jnp.float32
         )
         # fmt: on
 
         # repeat smatrix for each wavelength since its frequency independent
-        return jnp.repeat(smatrix, len(wl), axis=0)
+        return jnp.stack([smatrix]*len(wl), axis=0)
 
 
 class Waveguide(Model):
@@ -79,5 +107,4 @@ class Heater(Model):
 if __name__ == "__main__":
     # Create a coupler
     coupler = Coupler()
-
-    coupler.s_params([1.5, 1.6])
+    print(coupler.s_params(jnp.array([0])))
