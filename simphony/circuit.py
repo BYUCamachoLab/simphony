b322828f33131392c38c74817ae584583e5cf8ee
Sequoia Ploeg <sequoiap4@gmail.com>
2023-07-13 20:20:42 +0100

    Major rework on circuit class

--- simphony/circuit.py
+++ simphony/circuit.py
@@ -2,13 +2,20 @@
 
 from __future__ import annotations
 
+import logging
 from collections import defaultdict
+from copy import copy, deepcopy
 from itertools import count
-from typing import List, Optional, Set, Tuple, Union
+from typing import TYPE_CHECKING, List, Optional, Set, Tuple, Union
 
 from simphony.connect import connect_s, vector_innerconnect_s
 from simphony.exceptions import ModelValidationError
 from simphony.models import EPort, Model, OPort, Port
+
+if TYPE_CHECKING:
+    from simphony.simulation.simdevices import SimDevice
+
+log = logging.getLogger(__name__)
 
 
 class Circuit(Model):
@@ -40,68 +47,107 @@ class Circuit(Model):
     def __init__(self, name: str = None) -> None:
         self.name = name or "circuit"
 
-        # self._components = []  # list of model instances in the circuit
-
-        self._internal_oports: list[OPort] = []  # internal optical ports
-        self._internal_eports: list[EPort] = []  # internal electrical ports
-        self._oports: list[OPort] = []  # exposed optical ports
-        self._eports: list[EPort] = []  # exposed electrical ports
-
-        self._onodes: list[tuple[OPort, OPort]] = []  # optical connections
-        self._enodes: list[set[EPort]] = []  # electrical connections
-
-        self._next_oidx = count()  # netid iterator
-        self._next_eidx = count()  # netid iterator
+        self._components = []  # list of model instances in the circuit
+        self._onodes: list[tuple[OPort, OPort]] = []  # optical netlist
+        self._enodes: list[set[EPort]] = []  # electrical netlist
 
         self._sparams = None
         self.exposed = False
         self.sim_devices: list[SimDevice] = []
 
+    # def __copy__(self):
+    #     """Shallow copy the circuit."""
+    #     cls = self.__class__
+    #     result = cls.__new__(cls)
+    #     result.__dict__.update(self.__dict__)
+    #     return result
+
+    # def __deepcopy__(self, memo):
+    #     """Deep copy the circuit."""
+    #     cls = self.__class__
+    #     result = cls.__new__(cls)
+    #     memo[id(self)] = result
+    #     for k, v in self.__dict__.items():
+    #         setattr(result, k, deepcopy(v, memo))
+    #     return result
+
     @property
     def components(self) -> list[Model | Circuit]:
-        """Return a list of components (model instances) in the circuit.
-
-        Order in which models were added is not preserved.
+        """Return a list of components (model instances) in the circuit in the
+        order they were added.
 
         Returns
         -------
         list
             List of all model instances in the circuit.
         """
-        oinstances = [port._original_instance for pair in self._onodes for port in pair]
-        einstances = [wire._original_instance for node in self._enodes for wire in node]
-        return list(set(oinstances + einstances))
-        # return self._components
+        return self._components
 
-    def _update_ports(self, model1: Model | Circuit, model2: Model | Circuit) -> None:
-        """Update the internal list of ports in the circuit."""
-        if model1 not in self.components:
-            self._internal_oports.extend(model1._oports)
-            self._internal_eports.extend(model1._eports)
-        if model2 not in self.components:
-            self._internal_oports.extend(model2._oports)
-            self._internal_eports.extend(model2._eports)
+    @property
+    def _oports(self):
+        """Return a list of all unconnected optical ports in the circuit."""
+        return [
+            port
+            for comp in self._components
+            for port in comp._oports
+            if not port.connected
+        ]
+
+    @property
+    def _eports(self):
+        """Return a list of all unconnected electrical ports in the circuit."""
+        return [
+            port
+            for comp in self._components
+            for port in comp._eports
+            if not port.connected
+        ]
+
+    def port_info(self) -> str:
+        """Return a string containing information about the ports in the
+        circuit.
+
+        Returns
+        -------
+        str
+            String containing information about the ports in the circuit.
+        """
+        info = f"{self.name} port info:\n"
+        for comp in self._components:
+            info += f"{comp}:\n"
+            for port in comp._oports:
+                info += f"  {'|' if port.connected else 'O'} {port}\n"
+        return info
 
     def _connect_o(self, port1: OPort, port2: OPort) -> None:
         """Connect two ports in the internal netlist and update the connections
-        vairable on the ports themselves."""
-        self._update_ports(port1.instance, port2.instance)
+        variable on the ports themselves."""
+
+        if not any(port1.instance is comp for comp in self._components):
+            self._components.append(port1.instance)
+        if not any(port2.instance is comp for comp in self._components):
+            self._components.append(port2.instance)
+
         self._onodes.append((port1, port2))
         port1._connections.add(port2)
         port2._connections.add(port1)
 
     def _connect_e(self, port1: EPort, port2: EPort) -> None:
         """Connect two ports in the internal netlist and update the connections
-        vairable on the ports themselves."""
+        variable on the ports themselves."""
 
         def update_connections(enodes: set[EPort]):
             for eport in enodes:
-                eport._connections.update(enodes)
-                eport._connections.remove(eport)
+                eport._connections.update(enodes)  # add all other ports
+                eport._connections.remove(eport)  # remove self
 
-        self._update_ports(port1.instance, port2.instance)
+        if not any(port1.instance is comp for comp in self._components):
+            self._components.append(port1.instance)
+        if not any(port2.instance is comp for comp in self._components):
+            self._components.append(port2.instance)
+
+        # Check if EPort already has some connections in the netlist
         for i, eports in enumerate(self._enodes):
-            # EPort already has some connections in the netlist
             if port1 in eports or port2 in eports:
                 self._enodes[i].update([port1, port2])
                 update_connections(self._enodes[i])
@@ -111,13 +157,24 @@ class Circuit(Model):
         self._enodes.append({port1, port2})
         update_connections(self._enodes[-1])
 
-    def connect(self, port1: Model | Port, port2: Model | Port):
+    def connect(
+        self,
+        port1: Model | OPort | EPort,
+        port2: Model | OPort | EPort | list[Model | OPort | EPort],
+    ) -> None:
         """Connect two ports together and add to the internal netlist.
 
-        If a Model is passed, the next available optical port is inferred. If
+        If a ``Model`` is passed, the next available optical port is used. If
         no optical ports are available, the next available electronic port is
-        inferred. The type of ``port1`` is used to determine the type of
-        ``port2`` if an explicit port is not given.
+        used. In this way, the first argument can be thought about as an
+        "iterable". Thought about this way, this function acts similarly to the
+        ``zip`` function: connections are made between items of the first
+        argument and items of the second until one of the two is exhausted, at
+        which point remaining items in the longer list are ignored.
+
+        If ``port1`` is explicitly an optical or electronic port and the type
+        of ``port2`` is not given, the type of ``port1`` is used to infer the
+        type of ``port2``.
 
         Parameters
         ----------
@@ -203,53 +260,13 @@ class Circuit(Model):
             f"Ports must be optical (OPort), electronic (EPort), or a Model/Circuit (got '{type(port1)}' and '{type(port2)}')"
         )
 
-    def expose(
-        self,
-        ports: Port | list[Port] | None = None,
-        names: str | list[str] | None = None,
-    ) -> None:
-        """Expose ports to the outside world.
-
-        Parameters
-        ----------
-        ports : Port or list of Ports
-            The ports to expose. If not given, all unconnected ports are
-            exposed.
-        names : str or list of str
-            The names to assign to the ports. If not given, the names are
-            inferred from the port names.
-        """
-        new_ports = []
-        if ports is None:
-            ports = [port for port in self._internal_oports if not port.connected]
-            ports += [port for port in self._internal_eports if not port.connected]
-
-        if isinstance(ports, Port):
-            ports = [ports]
-        if names is None:
-            names = [port.name for port in ports]
-        if isinstance(names, str):
-            names = [names]
-        if len(ports) != len(names):
-            raise ValueError(
-                f"Number of ports ({len(ports)}) does not match number of names ({len(names)})"
-            )
-
-        new_ports = ports  # don't make new ports
-
-        new_oports = [port for port in new_ports if isinstance(port, OPort)]
-        new_eports = [port for port in new_ports if isinstance(port, EPort)]
-        self._oports = new_oports
-        self._eports = new_eports
-        self.exposed = True
-
     def __iter__(self):
         yield self
 
     def s_params(self, wl):
         """Compute the scattering parameters for the circuit.
 
-        Using the sub-network growth method.
+        Using the sub-network growth algorithm.
         """
         # TODO: What if a different wavelength range is passed in? This seems
         # like a bad idea.
@@ -257,42 +274,76 @@ class Circuit(Model):
             return self._sparams
 
         if len(self._onodes) == 0:
-            raise ModelValidationError("No devices in circuit.")
-        if not self.exposed:
-            self.expose()
+            raise ModelValidationError("No connections in circuit.")
 
         class STemp:
             def __init__(self, s, oports):
-                self.s = s
+                self._sparams = s
                 self._oports = oports
 
             def s_params(self, wl):
-                return self.s
+                return self._sparams
+
+        wl = tuple(wl)
 
         # Iterate through all connections and update the s-parameters
-        for cnx in self._onodes:
-            port1, port2 = cnx
+        for port1, port2 in self._onodes:
+            log.debug(
+                "Connecting %s from %s to %s from %s",
+                port1,
+                port1.instance,
+                port2,
+                port2.instance,
+            )
+            # innerconnect
             if port1.instance == port2.instance:
-                sparams = port1.instance.s_params(wl)
+                log.debug("Innerconnecting %s and %s", port1, port2)
+                sparams = (
+                    port1.instance._s(wl)
+                    if hasattr(port1.instance, "_s")
+                    else port1.instance.s_params(wl)
+                )
                 p1_idx = port1.instance._oports.index(port1)
                 p2_idx = port2.instance._oports.index(port2)
                 model = STemp(
                     vector_innerconnect_s(sparams, p1_idx, p2_idx),
                     port1.instance._oports,
                 )
+            # connect
             else:
-                p1_sparams = port1.instance.s_params(wl)
+                log.debug("Connecting %s and %s", port1, port2)
+                p1_sparams = (
+                    port1.instance._s(wl)
+                    if hasattr(port1.instance, "_s")
+                    else port1.instance.s_params(wl)
+                )
                 p1_idx = port1.instance._oports.index(port1)
-                p2_sparams = port2.instance.s_params(wl)
+                p2_sparams = (
+                    port2.instance._s(wl)
+                    if hasattr(port2.instance, "_s")
+                    else port2.instance.s_params(wl)
+                )
                 p2_idx = port2.instance._oports.index(port2)
                 model = STemp(
                     connect_s(p1_sparams, p1_idx, p2_sparams, p2_idx),
                     port1.instance._oports + port2.instance._oports,
                 )
-            model._oports.remove(port1)
-            model._oports.remove(port2)
-            port1._update_instance(model)
-            port2._update_instance(model)
 
-        self._sparams = model.s
+            model._oports = [
+                port
+                for port in model._oports
+                if port is not port1 and port is not port2
+            ]
+            for port in model._oports:
+                port.instance = model
+
+        # Make sure all remaining ports point to the same model, otherwise we
+        # have a disconnected circuit.
+        for port in self._oports:
+            if port.instance is not model:
+                raise ValueError(
+                    "Separated circuit: two or more disconnected subcircuits contained within the same circuit."
+                )
+
+        self._sparams = model._sparams
         return self._sparams
