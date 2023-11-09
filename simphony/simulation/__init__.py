9172c82b8a49fa10bc3d3320cb0dfa84264e270f
Sequoia Ploeg <sequoiap4@gmail.com>
2023-11-08 17:08:16 -0700

    Manually import from submodules for now

--- simphony/simulation/__init__.py
+++ simphony/simulation/__init__.py
@@ -1,6 +1,6 @@
 """Imports relevant classes and functions from the simulation module."""
 
-from simphony.simulation.classical import (  # LayoutAwareSim,; MonteCarloSim,; SamplingSim,; Simulation,; SimulationResult,; TimeDomainSim,
-    ClassicalResult,
-    ClassicalSim,
-)
+# from simphony.simulation.classical import (  # LayoutAwareSim,; MonteCarloSim,; SamplingSim,; Simulation,; SimulationResult,; TimeDomainSim,
+#     ClassicalResult,
+#     ClassicalSim,
+# )
