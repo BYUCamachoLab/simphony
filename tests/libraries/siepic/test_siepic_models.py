b4cf9011d5b37138b67b8e5f23fc9c36d9cd1a25
Sequoia Ploeg <sequoiap4@gmail.com>
2024-01-09 22:30:31 -0700

    Fix siepic models tests

--- tests/libraries/siepic/test_siepic_models.py
+++ tests/libraries/siepic/test_siepic_models.py
@@ -5,30 +5,28 @@ from simphony.libraries import siepic
 
 class TestBidirectional_coupler:
     def test_invalid_parameters(self):
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             siepic.bidirectional_coupler(pol="tem")
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             siepic.bidirectional_coupler(pol="te", thickness=200)
 
     def test_instantiable(self):
         siepic.bidirectional_coupler(thickness=220, width=500)
 
     def test_s_params(self, std_wl_um):
-        bdc = siepic.bidirectional_coupler()
-        bdc(std_wl_um)
+        s = siepic.bidirectional_coupler(wl=std_wl_um)
 
 
 class Testdirectional_coupler:
     def test_invalid_parameters(self):
-        with pytest.raises(ValueError):
+        with pytest.raises(FileNotFoundError):
             siepic.directional_coupler(gap=300)
 
     def test_instantiable(self):
         siepic.directional_coupler(gap=200, coupling_length=45)
 
     def test_s_params(self, std_wl_um):
-        dc = siepic.directional_coupler(gap=200, coupling_length=45)
-        dc(std_wl_um)
+        s = siepic.directional_coupler(wl=std_wl_um, gap=200, coupling_length=45)
 
 
 class Testgrating_coupler:
@@ -42,15 +40,14 @@ class Testgrating_coupler:
         siepic.grating_coupler(pol="te", thickness=220, dwidth=0)
 
     def test_s_params(self, std_wl_um):
-        gc = siepic.grating_coupler(pol="te")
-        gc(std_wl_um)
+        s = siepic.grating_coupler(wl=std_wl_um, pol="te")
 
 
 class Testhalf_ring:
     def test_invalid_parameters(self):
         with pytest.raises(ValueError):
             siepic.half_ring(pol="tem")
-        with pytest.raises(ValueError):
+        with pytest.raises(FileNotFoundError):
             siepic.half_ring(pol="te", gap=30, radius=10)
 
     def test_instantiable(self):
@@ -59,25 +56,29 @@ class Testhalf_ring:
         )
 
     def test_s_params(self, std_wl_um):
-        ring = siepic.half_ring(
-            pol="te", gap=50, radius=5, width=500, thickness=220, coupling_length=0
+        s = siepic.half_ring(
+            wl=std_wl_um,
+            pol="te",
+            gap=50,
+            radius=5,
+            width=500,
+            thickness=220,
+            coupling_length=0,
         )
-        ring(std_wl_um)
 
 
 class Testtaper:
     def test_invalid_parameters(self):
-        with pytest.raises(ValueError):
+        with pytest.raises(FileNotFoundError):
             siepic.taper(w1=0.3)
-        with pytest.raises(ValueError):
+        with pytest.raises(FileNotFoundError):
             siepic.taper(w2=0.5)
 
     def test_instantiable(self):
         siepic.taper(w1=0.5, w2=1.0, length=10.0)
 
     def test_s_params(self, std_wl_um):
-        taper = siepic.taper(w1=0.5, w2=1.0, length=10.0)
-        taper(std_wl_um)
+        s = siepic.taper(wl=std_wl_um, w1=0.5, w2=1.0, length=10.0)
 
 
 class Testterminator:
@@ -90,25 +91,24 @@ class Testterminator:
         siepic.terminator(pol="tm")
 
     def test_s_params(self, std_wl_um):
-        term = siepic.terminator(pol="te")
-        term(std_wl_um)
-        term = siepic.terminator(pol="tm")
-        term(std_wl_um)
+        s = siepic.terminator(wl=std_wl_um, pol="te")
+        s = siepic.terminator(wl=std_wl_um, pol="tm")
 
 
 class Testwaveguide:
     def test_invalid_parameters(self):
         with pytest.raises(ValueError):
             siepic.waveguide(pol="tem")
-        with pytest.raises(ValueError):
+        with pytest.raises(FileNotFoundError):
             siepic.waveguide(height=200)
 
     def test_instantiable(self):
         siepic.waveguide(pol="te", length=100, width=500, height=220, loss=2)
 
     def test_s_params(self, std_wl_um):
-        wg = siepic.waveguide(pol="te", length=100, width=500, height=220, loss=2)
-        wg(std_wl_um)
+        s = siepic.waveguide(
+            wl=std_wl_um, pol="te", length=100, width=500, height=220, loss=2
+        )
 
 
 class Testy_branch:
@@ -120,5 +120,4 @@ class Testy_branch:
         siepic.y_branch(pol="te", thickness=220, width=500)
 
     def test_s_params(self, std_wl_um):
-        yb = siepic.y_branch(pol="te")
-        yb(std_wl_um)
+        s = siepic.y_branch(wl=std_wl_um, pol="te")
