0445546ef6d107f6d3adc19dd91d4c4fe35ac0e3
11of12 <chandlerparkin@gmail.com>
2024-09-24 11:26:24 -0600

    Added utils function

--- simphony/utils.py
+++ simphony/utils.py
@@ -515,3 +515,26 @@ def resample(x: ArrayLike, xp: ArrayLike, sdict: sax.SDict) -> sax.SDict:
         cs = CubicSpline(xp, v)
         new_sdict[k] = cs(x)
     return new_sdict
+
+
+def smooth_rectangular_pulse(t, t_start, t_end, width=None):
+    """
+    A useful function for testing time-domain components
+
+    t = np.linspace(0, 1e-10, 1000)
+    pulse = smooth_rectangular_pulse(t, t_start=20e-12, t_end=40e-12)
+    plt.plot(t, pulse)
+    plt.show()
+    """
+    if width is None:
+        width = (t_end - t_start) / 100.0
+    # Transition functions
+    rise = 0.5 * (1 + jnp.tanh((t - t_start) / width))
+    fall = 0.5 * (1 + jnp.tanh((t_end - t) / width))
+    
+    # Smooth pulse
+    pulse = rise * fall
+    return pulse
+
+def gaussian_pulse(t, t0, std, a=1.0 ):
+    return a * jnp.exp(-(t - t0)**2 / std**2) 
