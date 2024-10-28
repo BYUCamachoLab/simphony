144b0725cab75a80d247a46b4caa2dc795d26f80
Matthew Worthington <matthewwor@gmail.com>
2024-10-28 01:49:55 -0600

    "adding the time_domain files to the time_domain branch they were in the master file "

---
+++ simphony/time_domain/pole_residue_model.py
@@ -0,0 +1,19 @@
+class PoleResidueModel():
+    def __init__(self) -> None:
+        pass
+    
+
+class CVFModel(PoleResidueModel):
+    def __init__(self) -> None:
+        pass
+
+class CVFModelBaseband(PoleResidueModel):
+    def __init__(self) -> None:
+        pass
+
+
+class IIRModelBaseband(PoleResidueModel):
+    def __init__(self) -> None:
+        pass
+
+
---
+++ simphony/time_domain/utils.py
@@ -0,0 +1,6 @@
+from simphony.time_domain import TimeSystem
+from simphony.time_domain import PoleResidueModel
+
+def pole_residue_to_time_system(poe_residue_model: PoleResidueModel) -> TimeSystem:
+    pass
+
