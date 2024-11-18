a09fcf46b2ae70a5a407934d14b42d06c23d3ad8
Matthew Worthington <matthewwor@gmail.com>
2024-11-18 09:41:42 -0700

    Current changes in the pole residue response

--- simphony/time_domain/time_system.py
+++ simphony/time_domain/time_system.py
@@ -3,6 +3,8 @@ from jax.typing import ArrayLike
 import numpy as np
 from scipy.signal import  StateSpace, lsim,dlsim
 from simphony.time_domain.pole_residue_model import PoleResidueModel
+import jax.numpy as jnp
+
 
 class TimeSystem(ABC):
     def __init__(self) -> None:
@@ -14,34 +16,38 @@ class TimeSystem(ABC):
         pass
 
 class CVF_Baseband_to_time_system(TimeSystem):
-    def __init__(self, pole_model: PoleResidueModel) -> None:
+    def __init__(self, pole_model: PoleResidueModel, t) -> None:
         self.A,self.B,self.C,self.D = pole_model.compute_state_space_model()
+        self.num_ports = pole_model.num_ports
+        self.t = t
         super().__init__()
 
-    def response(self, input: ArrayLike) -> ArrayLike:
+    def response(self, inputs: dict) -> ArrayLike:
+        N = inputs['o0'].shape[0]
+        responses = {
+            'o0': jnp.zeros((N), dtype=complex)
+            }
+        for i in range(0,self.num_ports):
+            responses[f'o{i}'] = jnp.zeros((N), dtype=complex)
         
-        N = int(len(input))
-        T = 4e-11
-        t = np.linspace(0,T,N)
-
         sys = StateSpace(self.A,self.B,self.C,self.D)
 
-        t_out,y_out,_ = lsim(sys, input, t)
+        t_out,y_out,_ = lsim(sys, input, self.t)
         
+        for i in range(0,self.num_ports):
+             responses[f'o{i}'] = y_out[i]
 
         return t_out, y_out
 
 class IIRModelBaseband_to_time_system(TimeSystem):
-    def __init__(self, pole_model: PoleResidueModel) -> None:
+    def __init__(self, pole_model: PoleResidueModel, t) -> None:
         self.sys = pole_model.generate_sys_discrete()
+        self.t = t
         super().__init__()
 
-    def response(self, input: ArrayLike) -> ArrayLike:
-        N = int(len(input))
-        T = 4e-11
-        t = np.linspace(0,T,N)
-
-        t_out,y_out,_ = dlsim(self.sys, input, t)
+    def response(self, inputs: ArrayLike) -> ArrayLike:
+        
+        t_out,y_out,_ = dlsim(self.sys, inputs, self.t)
         
 
         return t_out, y_out
