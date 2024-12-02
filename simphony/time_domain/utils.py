73b16e3b3b92d59b9596f111c36b92fd36dcde6a
Matthew Worthington <matthewwor@gmail.com>
2024-12-02 10:23:55 -0700

    Again the changes made

--- simphony/time_domain/utils.py
+++ simphony/time_domain/utils.py
@@ -1,13 +1,13 @@
-from simphony.time_domain.time_system import TimeSystem, IIRModelBaseband_to_time_system, CVF_Baseband_to_time_system
+from simphony.time_domain.time_system import TimeSystem, IIRModelBaseband_to_time_system
 from simphony.time_domain.pole_residue_model import PoleResidueModel, IIRModelBaseband, CVFModelBaseband
 from jax.typing import ArrayLike
 import jax.numpy as jnp
 
-def pole_residue_to_time_system(pole_residue_model: PoleResidueModel,t) -> TimeSystem:
+def pole_residue_to_time_system(pole_residue_model: PoleResidueModel) -> TimeSystem:
     if isinstance(pole_residue_model, IIRModelBaseband):
-        return IIRModelBaseband_to_time_system(pole_residue_model,t)
-    if isinstance(pole_residue_model, CVFModelBaseband):
-        return CVF_Baseband_to_time_system(pole_residue_model,t)
+        return IIRModelBaseband_to_time_system(pole_residue_model)
+    # if isinstance(pole_residue_model, CVFModelBaseband):
+    #     return CVF_Baseband_to_time_system(pole_residue_model)
 
 
 
