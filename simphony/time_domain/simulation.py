d25b5529d961a9cf0f2d4f059d16d0c12893d862
Chandler Parkin <chandlerparkin@gmail.com>
2025-03-06 19:06:01 -0700

    Fixed bug with the beta value

--- simphony/time_domain/simulation.py
+++ simphony/time_domain/simulation.py
@@ -176,7 +176,7 @@ class TimeSim(Simulation):
             circuit_params = model_parameters
             s = circuit(**circuit_params)
             self.S_params_dict = np.asarray(dict_to_matrix(s))
-            model = IIRModelBaseband(wvl,center_wvl,self.S_params_dict, model_order)
+            model = IIRModelBaseband(wvl,center_wvl,self.S_params_dict, model_order, options=custom_options)
             self.time_system = TimeSystemIIR(model)
 
     
