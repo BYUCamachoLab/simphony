2a5fe97338a004d5fbf46469bb3c5c76e1a053f1
Michael Murdock <mm13000@byu.edu>
2025-04-09 17:00:51 -0600

    Small edits to meet flake8 standards and remove annoying flake8 requirements

--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,6 @@ from typing import get_origin
 def is_sdict(obj: object) -> bool:
     # Check base object type of SDict
     SDict_base_type = get_origin(SDict)
-    if not SDict_base_type is None:
+    if SDict_base_type is not None:
         return isinstance(obj, SDict_base_type)
     return isinstance(obj, type(SDict))
