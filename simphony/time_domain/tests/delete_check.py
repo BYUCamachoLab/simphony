30c19396dc703b0a4874f0f7983ee05c98c367b0
1400073 <matthewwor@gmail.com>
2025-05-08 12:20:53 -0600

    delete_checkstuff

--- simphony/time_domain/tests/delete_check.py
+++ simphony/time_domain/tests/delete_check.py
@@ -1,74 +1,79 @@
-import os
-import pickle
-import pytest
-import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
 import jax.numpy as jnp
+import sax
+import jax
 from jax import config
 config.update("jax_enable_x64", True)
-from simphony.time_domain.simulation import TimeSim,TimeResult
-from simphony.time_domain.utils import  gaussian_pulse, smooth_rectangular_pulse
-from simphony.libraries import siepic
-from simphony.time_domain.ideal import Modulator
-T = 2.0e-11
-dt = 0.5e-14      # Total time duration (40 ps)
-t = jnp.arange(0, T, dt)
+import os, sys
+sys.path.append("/Users/mw742/simphony")
+# 1. Where Python is “running” right now
+print("CWD:", os.getcwd())
 
-netlist={
-    "instances": {
-        "wg": "waveguide",
-        "wg2": "waveguide",
-        "y": "y_branch",
-        "hr":"half_ring",
-        "hr2":"half_ring",
-        "y2": "y_branch",
+# 2. All the places Python will search for modules
+print("sys.path:", sys.path)
+
+from simphony.time_domain import TimeCircuit, TimeSim
+from simphony.time_domain.utils import gaussian_pulse, smooth_rectangular_pulse
+from simphony.libraries import siepic,ideal
+from simphony.time_domain.ideal import Modulator
+
+
+import json
+
+
+netlist = {
+    "instances":{
+        "mmi": "MultiModeInterferometer",
+        # "wg": "waveguide",
+
+
     },
     "connections": {
-        "hr,port_1":"hr2,port_1",
-        "hr,port_3":"hr2,port_3",
+
 
     },
     "ports": {
-        "o0":"hr,port_2",
-        "o2":"hr2,port_4",
-        "o1":"hr,port_4",
-        "o3":"hr2,port_2",
+        "o0": "mmi,o0",
+        "o1": "mmi,o1",
+        "o2": "mmi,o2",
+        "o3": "mmi,o3",
+
+
+        # "o0": "wg,o0",
+        # "o1": "wg,o1",
     },
 }
-models={
-    "waveguide": siepic.waveguide,
-    "y_branch": siepic.y_branch,
-    "half_ring": siepic.half_ring,
-    "bidirectional": siepic.bidirectional_coupler,
+
+
+T = 1.0e-11
+dt = 1e-14                   # Time step/resolution
+t = jnp.arange(0, T, dt)
+models = {
+    "MultiModeInterferometer": ideal.MMI,
+    "waveguide": ideal.waveguide,
+    "coupler": ideal.coupler,
+    }
+num_measurements = 200
+wvl = np.linspace(1.5, 1.6, num_measurements)
+options = {
+    'wl': wvl, 'mmi': {'wl':wvl},
 }
 
 
-
-time_sim = TimeSim(
-    netlist=netlist,
-    models=models,
-    
-    )
-
-num_measurements = 200
-model_order = 50
-center_wvl = 1.548
-wvl = np.linspace(1.5, 1.6, num_measurements)
-options = {'wl':wvl,'wg':{"length": 10.0, "loss": 100}, 'wg2':{"length": 10.0, "loss": 100}}
-
-
-time_sim.build_model(model_parameters=options, dt = dt,max_size = 3)
-
-
-
-num_outputs = 4
-
+time_sim = TimeSim(netlist=netlist, models=models)
+local_I = jnp.array([])
+time_sim.build_model(model_parameters=options, center_wvl=1.55, dt=dt, max_size=2, suppress_output=True)
+num_outputs = len(time_sim.netlist['ports'])
 inputs = {
-            f'o{i}': smooth_rectangular_pulse(t,0.0e-11,4.0e-11) if i == 0 else jnp.zeros_like(t)
-            for i in range(num_outputs)
-        }
+    f'o{i}': smooth_rectangular_pulse(t, 0.01e-10, 0.25e-10) if i == 0 else jnp.zeros_like(t)
+    for i in range(num_outputs)
+}
+result = time_sim.run(t, inputs)
+result.plot_sim()
+outputs = result.outputs
+plt.plot(t, jnp.angle(outputs['o3']))
+plt.show()
 
 
-modelResult =time_sim.run(t, inputs)
-modelResult.plot_sim()
+
