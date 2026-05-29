b89374be2665d9e2d5d07c631053f06f7842ac57
Matthew Worthington <matthewwor@gmail.com>
2026-05-29 01:29:31 -0600

    Fix prerelease test imports

---
+++ simphony/libraries/ideal/__init__.py
@@ -0,0 +1,8 @@
+# Copyright © Simphony Project Contributors
+# Licensed under the terms of the MIT License
+# (see simphony/__init__.py for details)
+"""Ideal SAX-compatible circuit models."""
+
+from simphony.libraries.old_ideal import coupler, waveguide
+
+__all__ = ["coupler", "waveguide"]
